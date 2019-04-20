06ca37b1106f3c227f62eb12ce6b3ef6201b3116
Joe Keegan <jwkeeg@comcast.net>
2019-04-20 10:12:00 -0500

    full function of music.js

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,8 +1,10 @@
 var url = window.location.href;
 
+// commented code below used for debugging
+
 // var url = "https://aangl3r.github.io/Project1/inner.html/" +
 //     "#access_token=" +
-//     "BQAdEIHH2v_rGEwCLHwamky1QEEL9VzqwrnxxLOYVWPayr8BrL1wwfZWN7J7igntCA6gDa3kLJi9QSODnpbNI_bSGp5QTQ76-PyB88ng7AxTRcdBE2p5kWJamOf_mNgVm1DHWNamh0HrSJYl1DjllH_OHP7V18M" +
+//     "BQDXKGv40PNBY-67o5GckU9bCX1YLH02AvA7WLlTdtMRypfH8gAnQ-bxkHXtJuHG9wfNocYD_332NZfd0ed-PjKbeg34BumSrTk3UifYZV0cWT0VADA2B0Pgirg9QvucvQdURMH5s2Qflntc0kBho6rhaQBF3vMhnw5-Tohad0MGQFzfvApOSFmq2SH5" +
 //     "&token_type=Bearer&expires_in=3600";
 
 console.log(url.split("access_token="));
@@ -13,6 +15,61 @@ console.log("TCL: url", url);
 var token = url[0];
 
 var errExists = false;
+
+// Create function to populate playlist in DOM
+// tracks is an array of track ids per spotify api
+function populatePlaylist(tracks) {
+    // clear active playlist
+    $("tbody").empty();
+    var playlistIndex = 1;
+
+    // Use the tracks we receive to populate chosen playlist
+    for (var i = 0; i < tracks.length; i++) {
+
+        $.ajax({
+            url: "https://api.spotify.com/v1/tracks/" + tracks[i].id,
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer " + token
+            },
+            success: function (data) {
+
+                // create table row to hold data
+                var newSong = $("<tr>");
+
+                // create vars to hold info
+                var songName;
+                var songArtist;
+                var songDuration;
+
+                // retrieve song name, artist and duration
+                // and convert duration_ms to mm:ss
+                songName = data.name;
+                songArtist = data.artists[0].name;
+
+                var durationMS = data.duration_ms;
+                var durationS = durationMS / 1000;
+                var remainder = Math.round(durationS % 60);
+                if (remainder < 10) remainder = "0" + remainder;
+                songDuration = Math.floor(durationS / 60) + ":" + remainder;
+
+                // append the data to the new row
+                newSong.append(
+                    $(
+                        "<td scope='row'>" + playlistIndex + "</td>" +
+                        "<td>" + songName + "</td>" +
+                        "<td>" + songArtist + "</td>" +
+                        "<td>" + songDuration + "</td>"
+                    )
+                );
+
+                // append the row to the table
+                $("tbody").append(newSong);
+                playlistIndex++;
+            }
+        });
+    }
+}
 
 // Create function to grab music based on face data
 // features: float of overall mood (from 0.0 - 1.0) where 1.0 is maximum happy
@@ -25,6 +82,7 @@ function getPlaylist(mood, genres) {
 
     $.when(
 
+        // we first need to grab the user name to later add the playlist to their spotify
         $.ajax({
             url: "https://api.spotify.com/v1/me",
             method: "GET",
@@ -70,8 +128,10 @@ function getPlaylist(mood, genres) {
             queryURL += genres[i];
         }
 
+        // add mood value passed in as target valence search query
         queryURL += "&target_valence=" + mood;
 
+        // ajax call to grab 20 songs. We will also add them to the DOM's playlist here
         $.ajax({
             url: queryURL,
             headers: {
@@ -86,47 +146,14 @@ function getPlaylist(mood, genres) {
                 for (i = 0; i < tracks.length; i++) {
                     console.log(tracks[i].id);
                 }
+
+                populatePlaylist(tracks);
+
+                return tracks;
             }
         });
-
-    }).then(function () {
-
-        // Use the tracks we receive to populate chosen playlist
-
-        // add playlist to firebase
 
     });
 
 
 }
-
-/* var searchTerms = [];
-searchTerms.push("min_danceability=.8");
-searchTerms.push("min_energy=.8");
-
-var queryURL = "https://api.spotify.com/v1/recommendations?limit=5&market=ES&seed_genres=country";
-
-// for (var i = 0; i < searchTerms.length; i++) {
-//     if (i === 0) queryURL += "?";
-//     else queryURL += "&";
-//     queryURL += searchTerms[i];
-// }
-
-console.log("TCL: queryURL", queryURL);
-
-$.ajax({
-    url: queryURL,
-    headers: {
-        "Authorization": "Bearer " + token
-    },
-    error: function (err) {
-        console.log("you done messed up");
-    },
-    success: function (data) {
-        console.log(data);
-        var tracks = data.tracks;
-        for (i = 0; i < tracks.length; i++) {
-            console.log(tracks[i].id);
-        }
-    }
-}); */
